e02ee5fcd367c042f75edcbbb37aa884733ab549
Fernanda-Marinho <fmarinho1918@gmail.com>
2023-08-15 00:02:59 -0300

    palavras reservadas

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,4 +1,4 @@
-reservadas = ["var","const", "class", "method","object", "main", "return", "if", "else", "then","for","read", "print", "void", "int", "real","boolean", "string", "true", "false"]
+reservadas = ["variables","const", "class", "methods", "objects", "main", "return", "if", "else", "then", "for","read", "print", "void", "int", "real", "boolean", "string", "true", "false"]
 digito = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 letra = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 aritmeticos = ["+", "-", "/", "*", "++", "--"]
@@ -7,7 +7,7 @@ logicos = ["!", "&&", "||"]
 delimitadores = [";", ",", ".", "(", ")", "[", "]", "{", "}", "->"]
 espaco = [" ", "\t","\n"]
 
-file = open('01.txt')
+file = open('exemplos/01.txt')
 
 
 '''entrada: handler de arquivo
@@ -17,9 +17,16 @@ def leitura (arquivo):
     acumulador = ''
     tokens = []
     n_linha = 0
-    for linha in arquivo:
+    for linha in arquivo: 
         n_linha +=1
-        for char in linha:
-            print(f'{char}\n')
+        for char in linha:  #quando EOF ele não entra no loop
+            if char.isspace() or char in espaco:    
+                if acumulador in reservadas:
+                    tokens.append(acumulador)
+                acumulador = ''
+            else:
+                acumulador += char
+    print(tokens)
+
 
 leitura(file)
