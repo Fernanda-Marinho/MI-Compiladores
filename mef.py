a7102c66755d036c6df70437f40e012be07edd72
amandassa <amandassa504@gmail.com>
2023-08-27 23:21:49 -0300

    update TMF

--- mef.py
+++ mef.py
@@ -97,7 +97,7 @@ def start (n_line, line):
                 elif line[i_curr] == '"':
                     token["ac"] += line[i_curr]
                     token["state"] = 7 
-                elif isErrTMF(line[i_curr]):
+                elif not isInRange(line[i_curr]):
                     token["ac"] = line[i_curr]
                     write_token(n_line,token["ac"],'TMF',errors_tokens)
                     clear_token(token)
