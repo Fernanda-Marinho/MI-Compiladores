75814f30fc77566c7854065012a85ef71ef5a30c
Fernanda-Marinho <fmarinho1918@gmail.com>
2023-09-01 17:23:41 -0300

    novas palavras reservadas

--- inspections.py
+++ inspections.py
@@ -1,5 +1,5 @@
 import re
-PRE = ["variables","const", "class", "methods", "objects", "main", "return", "if", "else", "then", "for","read", "print", "void", "int", "real", "boolean", "string", "true", "false"]
+PRE = ["variables","const", "class", "methods", "objects", "main", "return", "if", "else", "then", "for","read", "print", "void", "int", "real", "boolean", "string", "true", "false", "this", "constructor", "extends"]
 DIG = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 LET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 ART = ["+", "-", "/", "*", "++", "--"]
